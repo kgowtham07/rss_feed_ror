42eb93698e16705be7d767958099cc2a06a1ce77
Kiran Gowtham <bijugautam07@gmail.com>
2018-12-24 17:05:16 +0530

    Basic Login Funtionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/signup',   to: 'users#new'
   root 'static_page#home'
   get  '/help',    to: 'static_page#help'
   get  '/about',   to: 'static_page#about'
   get  '/contact', to: 'static_page#contact'
   post '/signup',  to: 'users#create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
