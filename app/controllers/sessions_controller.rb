3033ae632045e89a04ccc1af789c56510a653a23
kgowtham07 <bijugautam07@gmail.com>
2019-01-04 15:42:49 +0530

    img src change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or root_url
+      if admin_user?
+        redirect_to showhub_path
+      else
+        redirect_back_or root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
